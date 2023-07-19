890bb9359f7fa07fdaa27dfd0b06e9932eb69516
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2023-07-19 22:32:05 +0530

    Create 268. Missing Number.cpp

---
+++ 268. Missing Number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+
+//strive yt
+//another way is xor
+int n=nums.size();
+
+//phle natural number ka total sum nikal liya
+int sum=(n*(n+1))/2;
+
+//fir array wale number ka sum
+int sum2=0;
+ for(int i=0;i<n;i++){
+     sum2=sum2+nums[i];
+
+
+ }  
+ //1 to n range wale ke sum - array ke elements ka sum
+
+ int p=sum-sum2;
+ return p;
+        
+    }
+};
