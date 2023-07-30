a1cff6ce0fe0ae6605c4563fd0cd820e0078b890
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2023-07-30 21:27:54 +0530

    Create 1672. Richest Customer Wealth.cpp

---
+++ 1672. Richest Customer Wealth.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int maxwealth=0;
+        for(int i=0;i<accounts.size();i++){
+            int wealth=0;
+            for(int j=0;j<accounts[0].size();j++){
+                wealth=wealth+accounts[i][j];
+
+            }
+            maxwealth=max(wealth,maxwealth);
+        }
+        return maxwealth;
+    }
+};
