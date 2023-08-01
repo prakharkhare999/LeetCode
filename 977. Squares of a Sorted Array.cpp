246041352262caa49c7f9cd0e52aac9219d3ff9d
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2023-08-01 08:51:36 +0530

    Create 977. Squares of a Sorted Array.cpp

---
+++ 977. Squares of a Sorted Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        //self implimented
+        vector<int>v;
+        for(int i=0;i<nums.size();i++){
+            nums[i]=pow(nums[i],2);
+            v.push_back(nums[i]);
+        }
+        sort(v.begin(),v.end());
+        return v;
+
+    }
+
+};
