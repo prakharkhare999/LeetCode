71b2819f9b51158e4a6038d9b615df0172d1a4bf
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2023-08-29 23:32:10 +0530

    Create 1470. Shuffle the Array.cpp

---
+++ 1470. Shuffle the Array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+
+//self impliment
+vector<int> v;
+
+
+      int i=0;
+      int j=n;
+      
+while(i<n){
+    v.push_back(nums[i]);
+    v.push_back(nums[j]);
+    i++;
+    j++;
+    
+
+
+        
+ } 
+ 
+          return v;
+      }
+
+};
