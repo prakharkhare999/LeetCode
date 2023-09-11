d71a63129d64ad210b92f0a05ab0c6e2240cb4d7
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2023-09-11 17:55:07 +0530

    Create 728. Self Dividing Numbers.cpp

---
+++ 728. Self Dividing Numbers.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> result;
+        for(int num = left; num <= right; num++){
+            bool selfDividing = true;
+            int workNum = num;
+            while(workNum){
+                //n-th digit is 0 or n-th digit of "num" cannot divide "num"
+                if(workNum%10==0 || num%(workNum%10)!=0){
+                    selfDividing = false;
+                    break;
+                }
+                //if workNum<10, there won't be next iteration
+                workNum/=10;
+            }
+            if(selfDividing) result.push_back(num);
+        }
+        return result;
+    }
+};
