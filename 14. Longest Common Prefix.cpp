aa73f816fcb464c59d547c7f8604a2c1db168f33
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2023-09-14 23:53:08 +0530

    Update 14. Longest Common Prefix.cpp

--- 14. Longest Common Prefix.cpp
+++ 14. Longest Common Prefix.cpp
@@ -1,4 +1,56 @@
+//easy solution
+
 class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans;
+        int i=0;
+        while(true){
+            char curr_ch=0;
+            for(auto str:strs){
+                if(i>=str.size()){
+                    //out of bound
+                    curr_ch=0;
+                    break;
+                }
+                //just started
+                if(curr_ch==0){
+                    curr_ch=str[i];
+                }
+                else if(str[i]!=curr_ch){
+                    curr_ch=0;
+                    break;
+
+                }
+
+            }
+            if(curr_ch==0){
+                break;
+            }
+            ans.push_back(curr_ch);
+            i++;
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
                 if(strs.size() == 0) return "";
@@ -14,3 +66,4 @@ public:
 
     }
 };
+*/
