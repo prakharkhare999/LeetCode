62c4594d8faf29866252e3da7455ffff96047630
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2023-09-23 13:24:23 +0530

    Update 48. Rotate Image.cpp

--- 48. Rotate Image.cpp
+++ 48. Rotate Image.cpp
@@ -1,5 +1,47 @@
 class Solution {
 public:
+//trick
+    void rotate(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        int n=m;//same size
+        //transpose
+        for(int i=0;i<m;i++){
+            for(int j=i;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        //reverse
+        for(int i=0;i<m;i++){
+        reverse(matrix[i].begin(),matrix[i].end());
+        }
+
+
+        
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*class Solution {
+public:
     void rotate(vector<vector<int>>& matrix) {
         for(int i=0;i<matrix.size();i++){
             for(int j=i+1;j<matrix[i].size();j++){
@@ -18,3 +60,4 @@ public:
         
     }
 };
+*/
