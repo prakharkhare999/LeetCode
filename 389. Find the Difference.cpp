160d6a2454118894df5bf4200c6656b9d06a6ee1
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2023-09-25 12:23:13 +0530

    Update 389. Find the Difference.cpp

--- 389. Find the Difference.cpp
+++ 389. Find the Difference.cpp
@@ -1,22 +1,61 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char, int> sm;
-        unordered_map<char, int> tm;
-        
-        for (auto c:s) {
-            sm[c]++;
+        //approach 1
+        //1 map
+      
+      
+      
+       /*
+        unordered_map<char,int>mp;
+        for(auto &ch:s){
+            mp[ch]++;
         }
-        for (auto c:t) {
-            tm[c]++;
-        }
-
-        for (auto c:tm) {
-            if (sm[c.first] < c.second) {
-                return c.first;
+        for(auto &ch:t){
+            if(mp.find(ch)==mp.end()){
+                return  ch;
             }
         }
+        return 'x';//never come here
+        */
 
-        return ' ';
+
+
+       //approach 2
+        //   find sun of each string and take the diff the diff is ans
+       
+       /*  int sum_s=0;
+         for(char &ch:s){
+             sum_s+=ch;
+         }
+
+          int sum_t=0;
+         for(char &ch:t){
+             sum_t+=ch;
+         }
+         //sum_t jayada hoga because it contain a extra character
+        return (char)(sum_t-sum_s);
+
+*/
+
+
+//approach 3
+        //xor
+
+int Xor=0;
+for(char &ch:s){
+    Xor^=ch;
+}
+for(char &ch:t){
+    Xor^=ch;
+}
+return (char)Xor;
+
+
+
+
+
+
+   
     }
 };
