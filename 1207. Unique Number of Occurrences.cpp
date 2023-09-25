732ab8bb1017a509193191ee60d187ec6a64ba4b
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2023-09-25 23:49:42 +0530

    Create 1207. Unique Number of Occurrences.cpp

---
+++ 1207. Unique Number of Occurrences.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        //but a another solution exist
+        unordered_map<int,int>mp;
+        unordered_set<int>set;
+        for(auto &i:arr){
+            mp[i]++;
+
+        }
+
+        for(auto &it:mp){
+            int fre=it.second;
+            if(set.find(fre)!=set.end()){
+                return false;
+
+            }
+set.insert(fre);
+        }
+
+return true;
+
+        
+    }
+};
