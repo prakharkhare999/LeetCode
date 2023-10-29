b33c6906ae1d9a29510572cfb2f821d4651249a0
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2023-10-29 23:09:38 +0530

    Create 458. Poor Pigs.cpp

---
+++ 458. Poor Pigs.cpp
@@ -0,0 +1,19 @@
+
+//Approach-1
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int T = minutesToTest/minutesToDie + 1;
+
+        int pigs = 0;
+
+        while(pow(T, pigs) < buckets) {
+            pigs++;
+        }
+
+
+        return pigs;
+
+        
+    }
+};
