988c6e2b9a566caa6bd908fed69c10c853200700
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2023-11-08 14:41:17 +0530

    Update 876. Middle of the Linked List.cpp

--- 876. Middle of the Linked List.cpp
+++ 876. Middle of the Linked List.cpp
@@ -8,6 +8,40 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if(head==NULL){
+            return head;
+        }
+        if(head->next==NULL){
+            return head;
+        }
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+
+        }
+        return slow;
+
+        
+    }
+};
+
+/*
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
@@ -35,3 +69,4 @@ public:
         
     }
 };
+*/
