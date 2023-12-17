f8f9ac00bdf148a6e2155a8658152f623f7caf1e
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2023-12-17 20:59:16 +0530

    Create 2353. Design a Food Rating System.cpp

---
+++ 2353. Design a Food Rating System.cpp
@@ -0,0 +1,43 @@
+ class FoodRatings {
+public:
+    unordered_map<string, set<pair<int, string>>> cuisine_ratings_food;
+    unordered_map<string, string> food_cuisine;
+    unordered_map<string, int> food_rating;
+    
+    FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
+        int n = foods.size();
+        
+        for(int i = 0; i<n; i++) {
+            string food    = foods[i];
+            string cuisine = cuisines[i];
+            int rating     = ratings[i];
+            
+            cuisine_ratings_food[cuisine].insert({-rating, food});
+            food_cuisine[food] = cuisine;
+            food_rating[food]  = rating;
+            
+            
+        }
+    }
+    
+    void changeRating(string food, int newRating) {
+        string cuisine = food_cuisine[food];
+        int oldRating  = food_rating[food];
+        
+        cuisine_ratings_food[cuisine].erase({-oldRating, food});
+        cuisine_ratings_food[cuisine].insert({-newRating, food});
+        food_rating[food] = newRating;
+        
+    }
+    
+    string highestRated(string cuisine) {
+        return begin(cuisine_ratings_food[cuisine])->second; 
+        
+        //first item will have highest absolute rating "we put all ratings in negative" (ordered_set)
+        //Why did we not put them in ascending order and just return the last element of ordered_set ?
+        //Ans : Because We can have a case like below
+        //{(15, abc), (16, ramen), (16, sushi)} -> Order in ordered_set. last element is not lexicographically smaller
+        //{(-16, ramen), (-16, sushi), (-15, abc)} -> First element is now lexicographically smaller
+    }
+};
+
