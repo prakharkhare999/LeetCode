880379f0215e461e0a277545eb688bd349554a3e
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2024-01-01 23:51:16 +0530

    Create 455. Assign Cookies.cpp

---
+++ 455. Assign Cookies.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int n=g.size();
+
+        int m=s.size();
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+int i=0;
+int j=0;
+int cnt=0;
+while(i<n && j<m){
+    if(s[j] >= g[i]){
+        i++;
+        cnt++;
+    }
+    j++;
+}
+        
+        return cnt;
+    }
+};
