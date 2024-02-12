a2d478f349ce25a8252245066929bb11a85edbfa
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2024-02-12 20:27:21 +0530

    Update 169. Majority Element.cpp

--- 169. Majority Element.cpp
+++ 169. Majority Element.cpp
@@ -24,7 +24,23 @@ public:
     }
 };
 
-
+//map
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        int n=nums.size();
+        for(auto &i:nums){
+            mp[i]++;
+        }
+        for(auto &i:mp){
+            if(i.second>(n/2)){
+                return i.first;
+            }
+        }
+        return -1;
+    }
+};
 
 
 
