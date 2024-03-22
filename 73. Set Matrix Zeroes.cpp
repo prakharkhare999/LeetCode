f8493a5821fd83a12d827843e7cccef53db35734
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2024-03-22 22:36:29 +0530

    Update 73. Set Matrix Zeroes.cpp

--- 73. Set Matrix Zeroes.cpp
+++ 73. Set Matrix Zeroes.cpp
@@ -48,3 +48,37 @@ public:
         }
     }
 };
+
+
+//cpp
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        vector<int>row;
+        vector<int>column;
+       
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix[i].size();j++){
+                if(matrix[i][j]==0){
+                    row.push_back(i);
+                    column.push_back(j);
+                }
+            }
+        }
+       
+        for(auto i:row){
+            for(int j=0;j<matrix[i].size();j++){
+                matrix[i][j] = 0;
+            }
+        }
+       
+       
+       
+        for(int i=0;i<matrix.size();i++){
+            for(auto j:column){
+                matrix[i][j] = 0;
+            }
+        }
+       
+    }
+};
