beceb94cd6e603c04831455865338ce33d9cec34
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2024-03-23 23:17:20 +0530

    Update 205. Isomorphic Strings.cpp

--- 205. Isomorphic Strings.cpp
+++ 205. Isomorphic Strings.cpp
@@ -17,3 +17,40 @@ public:
         return true;
     }
 };
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+         if (s.length() != t.length()) {
+            return false;
+        }
+
+        // Create an unordered_map to store character mappings
+        std::unordered_map<char, char> charMappingMap;
+
+        for (int i = 0; i < s.length(); i++) {
+            char original = s[i];
+            char replacement = t[i];
+
+            if (charMappingMap.find(original) == charMappingMap.end()) {
+                // If original character not in map, check if replacement is already mapped
+                auto it = charMappingMap.begin();
+                while (it != charMappingMap.end()) {
+                    if (it->second == replacement) {
+                        return false; // Replacement character already mapped to another original character
+                    }
+                    it++;
+                }
+                // If replacement is not already mapped, add the mapping
+                charMappingMap[original] = replacement;
+            } else {
+                // If original character is already in map, check if its mapping matches the current replacement
+                if (charMappingMap[original] != replacement) {
+                    return false; // Mismatch in mapping
+                }
+            }
+        }
+
+        return true;
+    
+    }
+};
