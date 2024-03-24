4e325b33f3e7fe86ee0d4ee24527d3ddb4a7e153
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2024-03-24 23:50:18 +0530

    Update 1. Two Sum.cpp

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -17,3 +17,21 @@ public:
       return p;
     }
 };
+//map
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> numToIndex;
+    vector<int> result;
+    
+    for (int i = 0; i < nums.size(); ++i) {
+        int complement = target - nums[i];
+        if (numToIndex.find(complement) != numToIndex.end()) {
+            result.push_back(numToIndex[complement]);
+            result.push_back(i);
+            break;
+        }
+        numToIndex[nums[i]] = i;
+    }
+    
+    return result;
+}
+
