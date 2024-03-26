c9cd3070a58c3cd150bf49e87f7804162233805f
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2024-03-26 23:23:53 +0530

    Update 242. Valid Anagram.cpp

--- 242. Valid Anagram.cpp
+++ 242. Valid Anagram.cpp
@@ -12,3 +12,30 @@ public:
         
     }
 };
+//map
+ class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return false;
+        unordered_map<char, int> m1;
+        unordered_map<char, int> m2;
+        for(int i=0;i<s.size();i++){
+            m1[s[i]]++;
+        }
+        for(int i=0;i<t.size();i++){
+            m2[t[i]]++;
+        }
+        for(auto x: m1){
+            char ch1=x.first;
+            int freq=x.second;
+            if(m2.find(ch1)!=m2.end()){
+                int freq2=m2[ch1];
+                if(freq!=freq2) return false;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
