1dd92ae1aad86ff06643bf6868947de8d47ce629
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2024-03-30 23:55:21 +0530

    Update 202. Happy Number.cpp

--- 202. Happy Number.cpp
+++ 202. Happy Number.cpp
@@ -21,3 +21,26 @@ public:
         return true;
     }
 };
+
+//unorderedset
+ unordered_set<int> usedIntegers;
+        while (true) {
+            // Find the sum of squares
+            int sum = 0;
+            while (n != 0) {
+                sum += std::pow(n % 10, 2.0);
+                n = n / 10;
+            }
+
+            // If sum is 1, return true
+            if (sum == 1) return true;
+
+            // Else, the new number is the current sum
+            n = sum;
+
+            // Check if we have already encountered that number
+            if (usedIntegers.find(n) != usedIntegers.end())
+                return false;
+            usedIntegers.insert(n);
+        }
+    }
