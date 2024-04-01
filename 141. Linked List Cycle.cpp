8d81d843ff15921875f8ca5cb548fae20f6b0040
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2024-04-01 22:49:54 +0530

    Update 141. Linked List Cycle.cpp

--- 141. Linked List Cycle.cpp
+++ 141. Linked List Cycle.cpp
@@ -30,3 +30,30 @@ public:
         
     }
 };
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head==NULL || head->next==NULL)return false;
+        ListNode* temp1=head->next->next;
+        ListNode* temp2=head;
+        while(temp1!=NULL){
+            if(temp1==temp2)return true;
+            if(temp1->next==NULL)return false;
+            temp1=temp1->next->next;
+            temp2=temp2->next;
+        }
+        return false;
+    }
+};
