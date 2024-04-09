3a213c80979b5d0ad5395bb4bdf9a58e32c54b0a
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2024-04-10 00:20:51 +0530

    Update 61. Rotate List.cpp

--- 61. Rotate List.cpp
+++ 61. Rotate List.cpp
@@ -8,6 +8,50 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+int getlength(ListNode* head){
+    int i=0;
+    ListNode *temp=head;
+    while(temp){
+        i++;
+        temp=temp->next;
+    }
+    return i;
+}
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head == NULL){
+            return NULL;
+        }
+        int len=getlength(head);
+        int acutuallength=(k%len);
+        if(acutuallength==0) return head;
+        int newheadposition=len-acutuallength-1;
+        ListNode* newlastnode=head;
+        for(int i=0;i<newheadposition;i++){
+            newlastnode=newlastnode->next;
+        }
+        ListNode* newhead=newlastnode->next;
+        newlastnode->next=0;
+        ListNode* it= newhead;
+        while(it->next){
+            it = it->next;
+        }
+it->next=head;
+return newhead;
+    }
+};
+//another
 class Solution {
 public:
     int getListSize(ListNode* head) {
