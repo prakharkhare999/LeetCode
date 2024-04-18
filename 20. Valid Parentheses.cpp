9f31128cbf60fb8483c5fa81c807df7bf6c720de
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2024-04-18 22:54:14 +0530

    Update and rename 20. Valid Parentheses to 20. Valid Parentheses.cpp

--- 20. Valid Parentheses.cpp
+++ 20. Valid Parentheses.cpp
@@ -89,3 +89,51 @@ public:
         return st.empty();
     }
 };
+//another
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st ; 
+        for (int i = 0 ;  i< s.length() ; i++)
+        {
+            char ch = s[i];
+
+
+            if (ch == '(' || ch == '{' || ch == '[')
+            {
+                st.push(ch) ; 
+            }
+
+            else {
+
+                if (!st.empty())
+                {
+                    char top = st.top() ;
+                    if ((ch == ')' && top == '(') || 
+                        (ch == '}' && top == '{') ||
+                        (ch == ']' && top == '[')) 
+                        {
+            
+                            st.pop() ;
+                        }
+                        else 
+                        {
+                            return false ; 
+                        }
+                }
+                else 
+                {
+
+                    return false ;
+                }
+            }
+        }
+
+ 
+        if (st.empty())
+        {
+            return true ; 
+        }
+        return false ;
+    }
+};
