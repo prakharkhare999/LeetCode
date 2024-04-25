b5d9157e38b1c55a2561cad2b1bf9a577a4480d2
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2024-04-25 23:39:09 +0530

    Create 509. Fibonacci Number.cpp

---
+++ 509. Fibonacci Number.cpp
@@ -0,0 +1,29 @@
+// memo
+class Solution {
+public:
+int solve(int n,vector<int>& dp){
+     if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+         if(dp[n]!=-1){
+            return dp[n];
+            }
+
+         return dp[n] = solve(n-1,dp)+solve(n-2,dp); 
+}
+    int fib(int n) {
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+        vector<int>dp(n+1,-1);
+       
+        return  solve(n,dp);
+
+    }
+};
