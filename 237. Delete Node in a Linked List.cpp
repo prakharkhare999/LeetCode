3512c473458a300f59457db81484a085fa08818e
Prakhar kumar khare <94421765+prakharkhare999@users.noreply.github.com>
2024-05-05 23:41:30 +0530

    Update 237. Delete Node in a Linked List.cpp

--- 237. Delete Node in a Linked List.cpp
+++ 237. Delete Node in a Linked List.cpp
@@ -30,3 +30,11 @@ public:
         
     }
 };
+ class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+        // *node = *node->next;
+    }
+};
